8a8dce0b8dfa23989801da72a4978f0db69ee14d
Michael Arvelo <michaeldevelopscode@gmail.com>
2021-01-21 14:48:08 -0500

    added get all, single(by id), and delete calls

--- server.js
+++ server.js
@@ -1,14 +1,76 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    
+    console.log('Connected to the election database.');
+});
 
 app.get('/', (req,res) => {
     res.json({
         message: 'Hello World'
+    });
+});
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
     });
 });
 
@@ -17,6 +79,9 @@ app.use((req,res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// start server after connection to db
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
